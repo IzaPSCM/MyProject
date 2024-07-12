d42e0c37494abb6533d7c5c311f078b09ec0c3e8
IzaPSCM <profa.izabelita@gmail.com>
2024-07-12 10:28:43 -0300

    Add files via upload

---
+++ MyDossie.py
@@ -0,0 +1,18 @@
+# Declaração de variáveis...
+MyName = "Izabelita P. da S. C. de Melo"
+MyHeigh = 1.61
+MyAge = 38
+MyTeam = "Flamengo"
+MyChildren = "Angelo e Maria Luiza"
+MyCity = "Recife"
+MyHome = "Rio de Janeiro"
+MyAccount = 1.79
+# Exibição do conteúdo das variáveis...
+print(f"Meu nome é {MyName}.")
+print(f"Minha altura é de {MyHeigh} metros")
+print(f"Minha idade é {MyAge} anos.")
+print(f"Meu time do coração é {MyTeam}.")
+print(f"Meus filhos se chamam {MyChildren}.")
+print(f"Cidade onde nasci: {MyCity}.")
+print(f"Cidade onde resido: {MyHome}.")
+print(f"Tenho na conta R${MyAccount}.")
