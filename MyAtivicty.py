c55c06e6178ad6514fa7f8d1ca7a0f6ae4e0e6f6
Izabelita P.da S.C. de Melo <profa.izabelita@gmail.com>
2024-07-17 10:40:47 -0300

    Alteração

---
+++ MyAtivicty.py
@@ -0,0 +1,44 @@
+# Construa um programa onde o usuário digitará dois números e o programa dará, como resultado, a soma dos dois números.
+
+X = int(input("Digite um número: "))
+Y = int(input("Digite um número: "))
+
+soma = X + Y
+print ("O resultado da operação é: ",  soma)
+
+# Construa um programa onde o usuário digitará um número e o programa escreverá, na tela,o resultado do quadrado deste número.
+
+X = int(input(" Digite um número: "))
+multiplicacao = X * X
+print ("O resultado da operação é: ", multiplicacao)
+
+# Construa um programa onde o usuário digitará dois números e o programa exibirá, na tela, o resultado das quatro operações básicas da matemática.
+
+X = int(input("Digite um número: "))
+Y = int(input(" Digite um número: "))
+
+soma = X + Y
+print ("O resultado da operação é: ", soma)
+
+subtracao = X - Y
+print ("O resultado da operação é: ", subtracao)
+
+divisao = X / Y
+print ("O resultado da operação é: ", divisao)
+
+multiplicacao = X * Y
+print ("O resultado da operação é: ", multiplicacao)
+
+# Construa um programa onde o usuário digitará o ano que nasceu e o programa irá escrever, na tela, quantos anos eles completará em 2040.
+
+X = int(input(" Digite o ano em que você nasceu: "))
+
+subtracao = 2040 - X
+print ("O resultado da operação é: ", subtracao)
+
+# Construa um programa onde o usuário digitará um número e o programa vai escrever, como resposta, o cubo deste valor.
+
+X = int(input( "Digite um número: "))
+
+multiplicacao = X * X * X 
+print (" O resultado da operação é: ", multiplicacao)
